ec6fa73d5366b6c274a6099c8db502fcc1ffd234
Kevin Cho <kevinhcho27@gmail.com>
2019-04-17 17:36:35 -0400

    Created UserNameDisplay component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
+import UserNameDisplay from "./UserNameDisplay"
 
 const App = () => {
 	return (
 		<div>
 			<h1>Industry Weapon Applicant Test</h1>
+			<div>
+				<UserNameDisplay />
+			</div>
 		</div>
 	)
 }
---
+++ src/UserNameDisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class UserNameDisplay extends Component {
+    constructor() {
+        super()
+    }
+
+    async componentDidMount() {}
+
+    render() {
+        return (
+            <div> HI IM WORKING </div>
+        )
+    }
+}
+
+export default UserNameDisplay
