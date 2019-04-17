ea733bcd99a137e204165ea33876e27e7f089673
Kevin Cho <kevinhcho27@gmail.com>
2019-04-17 17:45:03 -0400

    Fetched data from endpoint and got all names to display

--- src/UserNameDisplay.js
+++ src/UserNameDisplay.js
@@ -3,13 +3,32 @@ import React, { Component } from "react"
 class UserNameDisplay extends Component {
     constructor() {
         super()
+        this.state = {
+            user: []
+        }
     }
 
-    async componentDidMount() {}
+    async componentDidMount() {
+        try {
+            let fetchedData = await fetch("https://jsonplaceholder.typicode.com/users")
+            let jsonedData = await fetchedData.json()
+            this.setState({
+                user: jsonedData
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     render() {
         return (
-            <div> HI IM WORKING </div>
+            <div>
+                {this.state.user ? this.state.user.map(user => {
+                    return (
+                        <div key={user.id}>{user.name}</div>
+                    )
+                }) : null}
+            </div>
         )
     }
 }
